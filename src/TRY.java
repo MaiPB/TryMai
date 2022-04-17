eb4a7e884918dbb59c337433e3c893393dd7f00a
MaiPB <103893595+MaiPB@users.noreply.github.com>
2022-04-17 23:13:11 +0700

    Initial commit

---
+++ src/TRY.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TRY {
+    public static void main (String[]arg) {
+        int num1=1;
+        int num2=2;
+        int result= num1 + num2 ;
+        System.out.println("This is Athicha Calculater Beta");
+        System.out.println("...............................");
+        System.out.println("Result of "  +num1+ " + " +num2+ " = " + result);
+        }
+    }
